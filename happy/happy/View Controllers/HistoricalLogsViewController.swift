1a0827339189f179de92ae9a389cfa645aadf79c
Jackson Tubbs <decknot@Jacksons-MacBook-Pro.local>
2019-09-01 19:16:28 -0600

    Makes month label for section work

--- happy/happy/View Controllers/HistoricalLogsViewController.swift
+++ happy/happy/View Controllers/HistoricalLogsViewController.swift
@@ -14,9 +14,14 @@ class HistoricalLogsViewController: UIViewController{
     // MARK: - Outlets
     
     @IBOutlet weak var calendarCollectionView: UICollectionView!
+    @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var yearLabel: UILabel!
     
     // MARK: - Properties
     
+    var currentSection = 0
+    var numCellsInCurrentSection = 0
+    var numCellsNotInCurrentSection = 0
     
     // MARK: - Lifecycle
     
@@ -25,6 +30,8 @@ class HistoricalLogsViewController: UIViewController{
         calendarCollectionView.delegate = self
         calendarCollectionView.dataSource = self
         calendarCollectionView.isPagingEnabled = true
+        calendarCollectionView.showsVerticalScrollIndicator = false
+//        calendarCollectionView.
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -42,17 +49,36 @@ extension HistoricalLogsViewController: UICollectionViewDelegate, UICollectionVi
         
         cell.configure(indexPath: indexPath, calendar: calendarCollectionView)
         
+//        if indexPath.section == currentSection && numCellsInCurrentSection <= 42{
+//            numCellsInCurrentSection += 1
+//            print("In: \(numCellsInCurrentSection) Not: \(numCellsNotInCurrentSection) Section: \(indexPath.section)")
+//            numCellsNotInCurrentSection -= 1
+//            if numCellsNotInCurrentSection > 0 {
+//            }
+//        } else {
+//            numCellsNotInCurrentSection += 1
+//            numCellsInCurrentSection -= 1
+//            print("In: \(numCellsInCurrentSection) Not: \(numCellsNotInCurrentSection) Section: \(indexPath.section)")
+//        }
+//        if numCellsNotInCurrentSection > numCellsInCurrentSection {
+//            print("Change to section: \(indexPath.section)")
+//            numCellsInCurrentSection = 0
+//            numCellsNotInCurrentSection = 0
+//            currentSection = indexPath.section
+//        }
+        
         return cell
     }
     
+    
+    
     func numberOfSections(in collectionView: UICollectionView) -> Int {
 //        return CalendarHelper.shared.months.count
-        return 3
+        return CalendarHelper.shared.months.count
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 42
     }
-    
     
     
     // MARK: - Flowlayout
@@ -63,5 +89,22 @@ extension HistoricalLogsViewController: UICollectionViewDelegate, UICollectionVi
         return size
     }
     
+//    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+//        print("Did End Dragging")
+//    }
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+//        print("Did Begin Dragging")
+    }
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+
+    }
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let sectionHeight = scrollView.contentSize.height / CGFloat(CalendarHelper.shared.months.count)
+        let section = Int(round(scrollView.contentOffset.y  / sectionHeight))
+        if section >= 0 && section < CalendarHelper.shared.months.count {
+            monthLabel.text = CalendarHelper.shared.months[section].monthName
+            yearLabel.text = CalendarHelper.shared.months[section].year
+        }
+    }
     
 }
