7febc2a29adc1f79f286998c352fc90828696665
Jackson Tubbs <decknot@Jacksons-MacBook-Pro.local>
2019-09-02 12:52:09 -0600

    Creates edit rating interface

--- happy/happy/View Controllers/HistoricalLogsViewController.swift
+++ happy/happy/View Controllers/HistoricalLogsViewController.swift
@@ -17,23 +17,40 @@ class HistoricalLogsViewController: UIViewController{
     @IBOutlet weak var monthLabel: UILabel!
     @IBOutlet weak var yearLabel: UILabel!
     
+    @IBOutlet weak var ratingEditStackView: UIStackView!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var ratingLabelView: UIView!
+    @IBOutlet weak var ratingSlider: UISlider!
+    @IBOutlet weak var saveLogButton: UIButton!
+    @IBOutlet weak var actionsButton: UIButton!
+    
     // MARK: - Properties
+    
+    var rating = 5
     
     // Used to only scroll to the bottom when the view firsts loads.
     var scrolledToBottom = false
-    // Used to upadate the selecte dates value with the LogController
-    var selectedDate: Date?
     
+    var selectedDate: Date?
+    var currentLog: Log?
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         calendarCollectionView.delegate = self
         calendarCollectionView.dataSource = self
         calendarCollectionView.isPagingEnabled = true
         calendarCollectionView.showsVerticalScrollIndicator = false
+        
+        ratingEditStackView.isUserInteractionEnabled = false
+        ratingEditStackView.alpha = 0.5
+        ratingLabel.text = "Select Date"
+        ratingLabelView.layer.cornerRadius = ratingLabel.frame.height / 2
+        ratingSlider.thumbTintColor = .black
+        ratingSlider.minimumTrackTintColor = .black
+        saveLogButton.layer.cornerRadius = saveLogButton.frame.height / 2
+        actionsButton.layer.cornerRadius = actionsButton.frame.height / 2
         
         // Sets the month label and year label equal to todays date
         let date = Date()
@@ -56,6 +73,15 @@ class HistoricalLogsViewController: UIViewController{
         }
     }
     
+    // MARK: - Actions {
+    
+    @IBAction func ratingSliderValueChanged(_ sender: UISlider) {
+        rating = Int(round(sender.value))
+        ratingSlider.value = Float(rating)
+        updateViewsForRating()
+    }
+    
+    
     // MARK: - Custom Funcitons
     
     // Used because the most current date is at the bottom of the collections view. I set it to the bottom of the collection view because I think it is more intuitive to the user. Easier user experience
@@ -64,6 +90,19 @@ class HistoricalLogsViewController: UIViewController{
         let contentSize = calendarCollectionView.contentSize
         let targetContentOffset = CGPoint(x: 0, y: contentSize.height - calendarCollectionView.bounds.size.height)
         calendarCollectionView.setContentOffset(targetContentOffset, animated: false)
+    }
+    
+    func updateViewsForRating() {
+        ratingLabelView.backgroundColor = RatingColors.getColorFoInt(number: rating)
+        ratingLabel.text = "\(rating)"
+        ratingSlider.minimumTrackTintColor = RatingColors.getColorFoInt(number: rating)
+        ratingSlider.value = Float(rating)
+        ratingSlider.thumbTintColor = RatingColors.getColorFoInt(number: rating)
+        saveLogButton.backgroundColor = RatingColors.getColorFoInt(number: rating)
+        actionsButton.backgroundColor = RatingColors.getColorFoInt(number: rating)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     }
 } // End of class
 
@@ -79,6 +118,7 @@ extension HistoricalLogsViewController: UICollectionViewDelegate, UICollectionVi
         return 42
     }
     
+    // Cell For Item At
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = calendarCollectionView.dequeueReusableCell(withReuseIdentifier: "dateCell", for: indexPath) as? DateCollectionViewCell else {return UICollectionViewCell()}
         
@@ -87,20 +127,33 @@ extension HistoricalLogsViewController: UICollectionViewDelegate, UICollectionVi
         return cell
     }
     
+    // Did Select
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // Updates Selectedate. The cell has a did set on the isSelected and will update the view based on that
         selectedDate = CalendarHelper.shared.months[indexPath.section].days[CalendarHelper.shared.getIndexForRow(indexPath: indexPath)]
+        guard let selectedDate = selectedDate else {return}
+        currentLog = LogController.shared.getLogForDate(date: selectedDate)
+        if let currentLog = currentLog {
+            rating = Int(currentLog.rating)
+            updateViewsForRating()
+        }
+        let selectionFeedbackGenerator = UISelectionFeedbackGenerator()
+        selectionFeedbackGenerator.selectionChanged()
         
-        
+        ratingEditStackView.isUserInteractionEnabled = true
+        ratingEditStackView.alpha = 1
     }
     
     // MARK: - Flowlayout
     
+    // Set size of cell
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         // 7 cells in a row for 7 days, 6 rows for the calendars ability to sometimes reach 6 rows tall
         let size = CGSize(width: calendarCollectionView.frame.width / 7, height: calendarCollectionView.frame.height / 6)
         return size
     }
     
+    // Did Scoll. Primary funciton is to update the month and year label when sections are changed
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         // Sets the month label to the current month in view
         let sectionHeight = scrollView.contentSize.height / CGFloat(CalendarHelper.shared.months.count)
